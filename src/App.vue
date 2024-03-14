5e3b4d2717e215318490b2ef9cba551a82cd77d9
欧阳俊杰 <1012793262@qq.com>
2024-03-14 20:21:48 +0800

    表格修改
    表格修改

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <meta charset="utf-8">
       <title>数据中心可视化管理平台</title> 
     </head>
-    <footer class="footer" style="z-index: 10;">
+    <footer class="footer" style="z-index: 10; width:100%">
       <!-- Footer content -->
     </footer>
     <div class="loading" style="display: none;">
@@ -21,46 +21,59 @@
       </div>
 
       <ul class="clearfix" style="z-index: 10; height: inherit">
-        <li>
-          <div class="boxall" style="height: calc(58% )">
-            <div class="alltitle">室内温度预测</div>
+        <li style="width: 25%">
+          <div class="boxall" style="height: calc(25% )">
+            <div class="alltitle">室内环境监控</div>
             <div class=" boxnav " id="echarts4" style="user-select: none; z-index: -10;" _echarts_instance_="ec_1710235324242">
               <div 
-                style="position: relative; overflow: hidden; width: 406px; height: 131px; padding: 0px; margin: 0px; border-width: 0px; cursor: default;">
+                style="position: relative; overflow: hidden; width: 100%; height: 100%; padding: 0px; margin: 0px; border-width: 0px; cursor: default;">
                 <canvas
-                  style="position: absolute; left: 0px; top: 0px; width: 406px; height: 131px; user-select: none; padding: 0px; margin: 0px; border-width: 0px;"
+                  style="position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; user-select: none; padding: 0px; margin: 0px; border-width: 0px;"
                   data-zr-dom-id="zr_0" width="507" height="163">
                 </canvas>
               </div>
             </div>
           </div>
-          <div class="boxall" style="height: calc(42% )">
+
+          <div class="boxall" style="height: calc(37% )">
+            <div class="alltitle">室内温度预测</div>
+            <div class=" boxnav " id="echarts4" style="user-select: none; z-index: -10;" _echarts_instance_="ec_1710235324242">
+              <div 
+                style="position: relative; overflow: hidden; width: 100%; height: 100%; padding: 0px; margin: 0px; border-width: 0px; cursor: default;">
+                <canvas
+                  style="position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; user-select: none; padding: 0px; margin: 0px; border-width: 0px;"
+                  data-zr-dom-id="zr_0" width="507" height="163">
+                </canvas>
+              </div>
+            </div>
+          </div>
+          <div class="boxall" style="height: calc(38% )">
             <div class="alltitle">服务器能耗预测</div>
             <div class="boxnav" id="echarts3" style="user-select: none; position: relative;"
               _echarts_instance_="ec_1710235324241">
               <div
-                style="position: relative; overflow: hidden; width: 406px; height: 77px; padding: 0px; margin: 0px; border-width: 0px; cursor: default;">
+                style="position: relative; overflow: hidden; width: 100%; height: 100%; padding: 0px; margin: 0px; border-width: 0px; cursor: default;">
                 <canvas
-                  style="position: absolute; left: 0px; top: 0px; width: 406px; height: 77px; user-select: none; padding: 0px; margin: 0px; border-width: 0px;"
+                  style="position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; user-select: none; padding: 0px; margin: 0px; border-width: 0px;"
                   data-zr-dom-id="zr_0" width="507" height="96"></canvas></div>
               <div></div>
             </div>
           </div>
         </li>
 
-        <li>
+        <li style="width: 50%">
 
         </li>
 
-        <li>
+        <li style="width: 25%">
           <div class="boxall" style="height: calc(32.333% )">
             <div class="alltitle">服务器能耗优化对比</div>
             <div class="boxnav" id="echarts5" style="user-select: none; position: relative;"
               _echarts_instance_="ec_1710235324243">
               <div
-                style="position: relative; overflow: hidden; width: 406px; height: 48px; padding: 0px; margin: 0px; border-width: 0px;">
+                style="position: relative; overflow: hidden; width: 100%; height: 100%; padding: 0px; margin: 0px; border-width: 0px;">
                 <canvas
-                  style="position: absolute; left: 0px; top: 0px; width: 406px; height: 48px; user-select: none; padding: 0px; margin: 0px; border-width: 0px;"
+                  style="position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; user-select: none; padding: 0px; margin: 0px; border-width: 0px;"
                   data-zr-dom-id="zr_0" width="507" height="60"></canvas></div>
               <div></div>
             </div>
@@ -78,9 +91,9 @@
               style="height: calc(100% - 0.3rem); user-select: none; position: relative;"
               _echarts_instance_="ec_1710235324244">
               <div
-                style="position: relative; overflow: hidden; width: 411px; height: 63px; padding: 0px; margin: 0px; border-width: 0px; cursor: default;">
+                style="position: relative; overflow: hidden; width: 100%; height: 100%; padding: 0px; margin: 0px; border-width: 0px; cursor: default;">
                 <canvas
-                  style="position: absolute; left: 0px; top: 0px; width: 411px; height: 63px; user-select: none; padding: 0px; margin: 0px; border-width: 0px;"
+                  style="position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; user-select: none; padding: 0px; margin: 0px; border-width: 0px;"
                   data-zr-dom-id="zr_0" width="513" height="78"></canvas></div>
               <div>
               </div>
