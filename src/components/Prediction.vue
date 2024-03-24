e5a8c9c57ce994682fd5ad9de287b38cb3383547
cccchh <1013239741@qq.com>
2024-03-24 17:00:41 +0800

    改tooltip bug

--- src/components/Prediction.vue
+++ src/components/Prediction.vue
@@ -31,45 +31,61 @@ export default {
         tooltip: {
           trigger: 'axis',
           formatter: function (params) {
-            const time = params[0].value[0];
-            const realValue = params[0].value[1];
-            const predictedValue = params[1].value[1];
-            return time + '<br />' + '真实值: ' + realValue + '<br />' + '预测值: ' + predictedValue;
+            if (typeof params === 'undefined') {
+              return ''; // 如果params为undefined，则返回空字符串
+            }
+            console.log(params)
+
+            if (params.length >= 2) {
+              const time = params[1].value[0];
+
+              const predictedValue = params[1].value[1];
+              const realValue = params[0].value[1];
+              return time + '<br />' + '真实值: ' + realValue + '<br />' + '预测值: ' + predictedValue;
+
+            }
+            else {
+              const time = params[0].value[0];
+
+              const predictedValue = params[0].value[1];
+              console.log(params)
+              return time + '<br />' + '真实值: -' + '<br />' + '预测值: ' + predictedValue;
+            }
           },
 
           axisPointer: {
             animation: false,
             type: 'shadow',
-							crossStyle: {
-								color: '#999'
-							},
+            crossStyle: {
+              color: '#999'
+            },
           },
-          position: function(point, params, dom, rect, size) {
-							var x = 0; // x坐标位置
-							var y = 0; // y坐标位置
-							// 当前鼠标位置
-						    var pointX = point[0];
-							var pointY = point[1];
-							// 提示框大小
-							var boxWidth = size.contentSize[0];
-							var boxHeight = size.contentSize[1];
- 
-							// boxWidth > pointX 说明鼠标左边放不下提示框
-							if (boxWidth > pointX) {
-								x = pointX + 10;
-							} else { // 左边放的下
-								x = pointX - boxWidth - 5;
-							}
- 
-							// boxHeight > pointY 说明鼠标上边放不下提示框
-							if (boxHeight > pointY) {
-								y = 5;
-							} else { // 上边放得下
-								y = pointY - boxHeight;
-							}
-							return [x, y];
-						},
-						
+          position: function (point, params, dom, rect, size) {
+            var x = 0; // x坐标位置
+            var y = 0; // y坐标位置
+            // 当前鼠标位置
+            var pointX = point[0];
+            var pointY = point[1];
+            // 提示框大小
+            var boxWidth = size.contentSize[0];
+            var boxHeight = size.contentSize[1];
+
+            // boxWidth > pointX 说明鼠标左边放不下提示框
+            if (boxWidth > pointX) {
+              x = pointX + 10;
+            } else { // 左边放的下
+              x = pointX - boxWidth - 5;
+            }
+
+            // boxHeight > pointY 说明鼠标上边放不下提示框
+            if (boxHeight > pointY) {
+              y = 5;
+            } else { // 上边放得下
+              y = pointY - boxHeight;
+            }
+            return [x, y];
+          },
+
         },
         grid: {
           top: '15%',
@@ -92,7 +108,7 @@ export default {
         },
         yAxis: {
           type: 'value',
-        
+
           splitLine: {
             show: false
           },
@@ -125,37 +141,79 @@ export default {
     setInterval(this.updateData, 1000);
   },
   methods: {
-    randomData() {
-      this.now = new Date(+this.now + 1000); // 更新为每秒
+    randomData(currentTime) {
+      // 获取当前时间
+      //var currentTime = new Date();
+
+
+      // 减去100秒
+      // currentTime.setSeconds(currentTime.getSeconds() - 100);
+
       this.value1 = this.value1 + Math.random() * 21 - 10;
       this.value2 = this.value2 + Math.random() * 21 - 10;
+
       return [
-        this.now.toLocaleTimeString(), // 修改为本地时间格式
+        currentTime.toLocaleTimeString(), // 修改为本地时间格式
+        // this.now.toLocaleTimeString(), // 修改为本地时间格式
         Math.round(this.value1),
         Math.round(this.value2)
       ];
     },
     initChart() {
-      for (var i = 0; i < 500; i++) { // 初始化14400个数据，即四个小时
-        const newData = this.randomData();
-        this.data1.unshift([newData[0], newData[1]]);
-        this.data2.unshift([newData[0], newData[2]]);
+      // 获取当前时间
+      var currentTime = new Date();
+      var currentTime1 = new Date();
+      currentTime.setSeconds(currentTime.getSeconds() - 100);
+      currentTime1.setSeconds(currentTime1.getSeconds() - 70);
+
+      // 减去100秒
+
+
+      for (var i = 0; i < 100; i++) { // 初始化100个数据点
+
+
+        var newData = this.randomData(currentTime); // 传入当前时间
+        var newData1 = this.randomData(currentTime1);
+        // newData[0].setSeconds(newData[0].getSeconds() - 1);
+        this.data1.push([newData[0], newData[1]]); // 在数组末尾添加新数据，保持数据顺序
+        this.data2.push([newData1[0], newData1[2]]); // 在数组末尾添加新数据，保持数据顺序
+        // newData[0].setSeconds(currentTime.getSeconds() - 1);
+        currentTime.setSeconds(currentTime.getSeconds() + 1); // 增加1秒，即移动到下一个时间点
+        currentTime1.setSeconds(currentTime1.getSeconds() + 1);
+        console.log(i);
+        console.log(newData[0], newData[1]);
+
       }
       this.option.series[0].data = this.data1;
       this.option.series[1].data = this.data2;
-      this.option.xAxis.data = this.data1.map(item => item[0]); // 使用时间数据作为x轴数据
+      this.option.xAxis.data = this.data2.map(item => item[0]); // 使用时间数据作为x轴数据
       this.myChart = echarts.init(document.getElementById('chartContainer'));
       this.myChart.setOption(this.option);
+
+      // 打印this.data1
+      console.log(this.data1);
     },
+
+
+
     updateData() {
-      this.data1.pop(); // 移除数组的最后一个元素
-      this.data2.pop(); // 移除数组的最后一个元素
-      const newData = this.randomData();
-      this.data1.unshift([newData[0], newData[1]]); // 在数组开头添加新数据，保持数据顺序
-      this.data2.unshift([newData[0], newData[2]]); // 在数组开头添加新数据，保持数据顺序
-      this.option.xAxis.data.pop(); // 移除x轴标签的最后一个元素
-      this.option.xAxis.data.unshift(newData[0]); // 在x轴标签的开头添加新的标签
+      var currentTime = new Date();
+      var currentTime1 = new Date();
+      currentTime1.setSeconds(currentTime1.getSeconds() + 30);
+      const newData = this.randomData(currentTime);
+      const newData1 = this.randomData(currentTime1);
+      this.data1.push([newData[0], newData[1]]); // 在数组末尾添加新数据，保持数据顺序
+      this.data2.push([newData1[0], newData1[2]]); // 在数组末尾添加新数据，保持数据顺序
+      this.data1.shift(); // 移除数组的第一个元素
+      this.data2.shift(); // 移除数组的第一个元素
+
+      this.option.xAxis.data.push(newData1[0]); // 在x轴标签的末尾添加新的标签
+      this.option.xAxis.data.shift(); // 移除x轴标签的第一个元素
+
       this.myChart.setOption({
+        xAxis: {
+          data: this.option.xAxis.data // 更新x轴标签配置
+        },
         series: [
           {
             data: this.data1
@@ -167,6 +225,8 @@ export default {
       });
     }
   }
+
+
 };
 </script>
 
