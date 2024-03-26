b3c76a6dd545f28cf8cdaf15fe1ab5f19e2ed7f5
欧阳俊杰 <1012793262@qq.com>
2024-03-26 09:42:21 +0800

    按钮修改

--- src/components/button_zhuye.vue
+++ src/components/button_zhuye.vue
@@ -1,22 +1,22 @@
 <template>
-  <div style="position: absolute; width: 150px; height: 50px; left: 50%; margin-left: -100px; bottom: 50px; z-index: 10;">
+  <div style="position: absolute; width: 200px; height: 50px; left: 50%; margin-left: -100px; bottom: 50px; z-index: 10;">
     <p v-if="view1">
-      <dv-border-box-12 backgroundColor="rgba(6, 48, 109, .5)">
-        <button class="rounded-btn" @click="handleClick" style="z-index: 20; width: 150px; height: 50px;">
-          <span style="font-size: 18px; font-weight: bold; color: azure;">
-            主页
+      <dv-border-box-13 backgroundColor="rgba(6, 48, 109, .5)">
+        <button class="rounded-btn" @click="handleClick" style="z-index: 20; width: 200px; height: 50px;">
+          <span style="font-size: 21px; font-weight: bold; color: azure;">
+            -主页-
           </span>
         </button>
-      </dv-border-box-12>
+      </dv-border-box-13>
     </p>
     <p v-else>
-      <dv-border-box-12>
-        <button class="rounded-btn" @click="handleClick" style="z-index: 20; width: 150px; height: 50px;">
-          <span style="font-size: 18px; font-weight: bold; color: azure;">
-            主页
+      <dv-border-box-13>
+        <button class="rounded-btn" @click="handleClick" style="z-index: 20; width: 200px; height: 50px;">
+          <span style="font-size: 21px; font-weight: bold; color: azure;">
+            -主页-
           </span>
         </button>
-      </dv-border-box-12>
+      </dv-border-box-13>
     </p>
   </div>
 </template>
